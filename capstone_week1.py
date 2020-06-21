99320f4b00e8336d86666783a517e604cac5e878
amishr12 <67219515+amishr12@users.noreply.github.com>
2020-06-21 13:24:55 +0530

    Add files via upload

---
+++ capstone_week1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+imprt pandas as pd
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
